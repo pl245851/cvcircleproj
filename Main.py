d1519e4303967bbe3819eb9820a8575d42ead70d
pl245851 <pl245851@ahschool.com>
2021-03-12 09:39:41 -0500

    works or something

--- Main.py
+++ Main.py
@@ -11,12 +11,19 @@ if circles is not None:
 	# convert the (x, y) coordinates and radius of the circles to integers
 	circles = np.round(circles[0, :]).astype("int")
 	# loop over the (x, y) coordinates and radius of the circles
+	maxx = 0
+	maxy = 0
+	maxr = 0
 	for (x, y, r) in circles:
 		print(x,y,r)
-		# draw the circle in the output image, then draw a rectangle
-		# corresponding to the center of the circle
-		cv2.circle(output, (x, y), r, (0, 165, 255), 4)
-		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (50, 50, 50), -1)
+		if r > maxr:
+			maxx = x
+			maxy = y
+			maxr = r
+	# draw the circle in the output image, then draw a rectangle
+	# corresponding to the center of the circle
+	cv2.circle(output, (maxx, maxy), maxr, (0, 165, 255), 4)
+	cv2.rectangle(output, (maxx - 5, maxy - 5), (maxx + 5, maxy + 5), (50, 50, 50), -1)
 	# show the output image
 	cv2.imshow("output", np.hstack([image, output]))
 	cv2.waitKey(0)
