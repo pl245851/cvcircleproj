6f94e36e0775fe60de147ab41c917910bcd14989
pl245851 <pl245851@ahschool.com>
2021-03-12 09:10:53 -0500

    works or something

--- Main.py
+++ Main.py
@@ -1,15 +1,18 @@
 import cv2
 import numpy as np
-image = cv2.imread("test_easy.jpg", cv2.IMREAD_COLOR)
+im = input("image file name\n")
+image = cv2.imread(im, cv2.IMREAD_COLOR)
 output = image.copy()
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+blur = cv2.GaussianBlur(gray,(5,5),0)
+circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1.2, 100)
 # ensure at least some circles were found
 if circles is not None:
 	# convert the (x, y) coordinates and radius of the circles to integers
 	circles = np.round(circles[0, :]).astype("int")
 	# loop over the (x, y) coordinates and radius of the circles
 	for (x, y, r) in circles:
+		print(x,y,r)
 		# draw the circle in the output image, then draw a rectangle
 		# corresponding to the center of the circle
 		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
